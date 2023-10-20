dc30773029fcaecb19da84ff0b101a093f46771d
vuhongson23 <sonvuvt123@gmail.com>
2023-10-20 10:27:03 +0700

    first commit

---
+++ style.js
@@ -0,0 +1,6 @@
+const btn_open = document.getElementById('no');
+const btn_close = document.getElementById('close');
+const container_model = document.getElementById('container_model');
+btn_open.addEventListener('click', ()=>{
+    alert('OK');
+})
